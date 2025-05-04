de7d0aec57362ab2d22379ecda1bfce42304c682
Liam Mulhall <liammulh@gmail.com>
2025-05-04 13:32:19 -0700

    test: Add test data

---
+++ src/tests/data/mondo.py
@@ -0,0 +1,16 @@
+"""Provide Mondo data for testing."""
+
+data = [
+    {
+        "id": "MONDO_0005052",
+        "label": "irritable bowel syndrome",
+    },
+    {
+        "id": "MONDO_0010030",
+        "label": "Sjogren syndrome",
+    },
+    {
+        "id": "MONDO_0005100",
+        "label": "systemic sclerosis",
+    },
+]
