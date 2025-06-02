cb003f942a2aa895b46795f46fbfd60b51cd9fa6
Liam Mulhall <liammulh@gmail.com>
2025-06-02 15:34:26 -0700

    feat(core): Add pages linked to in footer
    This commit adds the pages that are linked to in the footer.
    For more information, see the issue below.
    
    https://github.com/ClinGen/hla-curation-interfacae/issues/1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,15 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path("..", "..", "src").resolve()))
+
 project = "HCI"
 copyright = "Clinical Genome Resource"
 author = "The Stanford ClinGen Team"
 release = "0.1"
 version = "0.1.0"
 
-extensions = []
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
--- src/core/urls.py
+++ src/core/urls.py
@@ -4,4 +4,13 @@ from django.urls import path
 
 from core import views
 
-urlpatterns = [path("", views.home, name="home")]
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about", views.about, name="about"),
+    path("acknowledgements", views.acknowledgements, name="acknowledgements"),
+    path("citing", views.citing, name="citing"),
+    path("collaborators", views.collaborators, name="collaborators"),
+    path("contact", views.contact, name="contact"),
+    path("downloads", views.downloads, name="downloads"),
+    path("help", views.help_, name="help"),
+]
