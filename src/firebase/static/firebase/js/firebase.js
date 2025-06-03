3b44ea31672229a66534bfe8b8d2ed6cfaccc9d1
Liam Mulhall <liammulh@gmail.com>
2025-06-03 01:47:48 -0700

    wip: Add view for authenticating with Firebase

--- src/firebase/static/firebase/js/firebase.js
+++ src/firebase/static/firebase/js/firebase.js
@@ -1,5 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
+import {initializeApp} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD_TLzPJT3IxX59b_7raOyLka11kLYGYg0",
@@ -7,25 +11,50 @@ const firebaseConfig = {
     projectId: "som-clingen-projects",
     storageBucket: "som-clingen-projects.firebasestorage.app",
     messagingSenderId: "653902215137",
-    appId: "1:653902215137:web:d054a272e88ab9ca2644a0"
+    appId: "1:653902215137:web:d054a272e88ab9ca2644a0",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
+function getCsrfToken() {
+    return document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("csrftoken="))
+        ?.split("=")[1];
+}
+
 async function loginWithGoogle() {
     const provider = new GoogleAuthProvider();
     try {
         const result = await signInWithPopup(auth, provider);
-        console.log(await result.user.getIdToken());
-    }
-    catch (error) {
-        let errorMessage = "Something went wrong trying to log you in with Google.\n\n";
+        const idToken = await result.user.getIdToken();
+        const response = await fetch("/firebase/auth", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRFToken": getCsrfToken(),
+            },
+            body: JSON.stringify({
+                id_token: idToken,
+            }),
+        });
+        if (response.ok) {
+            const data = await response.json();
+            if (data.success) {
+                window.alert("Login with Google successful.");
+            }
+        }
+    } catch (error) {
+        let errorMessage =
+            "Something went wrong trying to log you in with Google.\n\n";
         errorMessage += `Error Code:\n${error.code}\n\n`;
         errorMessage += `Error Message:\n${error.message}`;
         window.alert(errorMessage);
     }
 }
 
-const loginWithGoogleButton = document.getElementById("login-with-google-button");
+const loginWithGoogleButton = document.getElementById(
+    "login-with-google-button",
+);
 loginWithGoogleButton.addEventListener("click", loginWithGoogle);
--- src/firebase/urls.py
+++ src/firebase/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from firebase import views
 
 urlpatterns = [
-    path("login", views.login, name="login"),
+    path("login", views.login_, name="login"),
+    path("auth/", views.auth, name="auth"),
 ]
