b7ffca6483b002791bec019a70503a6457fd0db9
Liam Mulhall <liammulh@gmail.com>
2025-06-05 15:24:07 -0700

    chore: Move setup into apps module
    Signed-off-by: Liam Mulhall <liammulh@gmail.com>

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -3,9 +3,7 @@
 import os
 from pathlib import Path
 
-import firebase_admin
 import sentry_sdk
-from firebase_admin import credentials
 
 # We use Sentry for error monitoring and tracing.
 sentry_sdk.init(
@@ -16,12 +14,6 @@ sentry_sdk.init(
 )
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-# We use Firebase for authentication.
-SERVICE_ACCOUNT_KEY_PATH = Path(BASE_DIR / "firebase-account-key.json")
-if not firebase_admin._apps:  # noqa: SLF001 (Avoid initializing multiple times in development.)
-    cred = credentials.Certificate(SERVICE_ACCOUNT_KEY_PATH)
-    firebase_admin.initialize_app(cred)
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
--- src/firebase/apps.py
+++ src/firebase/apps.py
@@ -1,9 +1,22 @@
 """Provides the configuration for the firebase app."""
 
+from pathlib import Path
+
+import firebase_admin
 from django.apps import AppConfig
+from firebase_admin import credentials
+
+from config.settings.base import BASE_DIR
 
 
 class FirebaseConfig(AppConfig):
     """Configures the firebase app."""
 
     name = "firebase"
+
+    def ready(self) -> None:
+        """Sets up the Firebase Admin SDK."""
+        service_account_key_path = Path(BASE_DIR / "firebase-account-key.json")
+        if not firebase_admin._apps:  # noqa: SLF001 (Avoid initializing multiple times in development.)
+            cred = credentials.Certificate(service_account_key_path)
+            firebase_admin.initialize_app(cred)
