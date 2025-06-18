4f5e8be08022471bb3b30793b6e2766abd513c3d
Liam Mulhall <liammulh@gmail.com>
2025-06-18 13:48:33 -0700

    tests: Move tests so they appear in tree order

--- src/firebase/tests.py
+++ src/firebase/tests.py
@@ -12,6 +12,85 @@ from firebase.clients import get_token_info
 from firebase.crud import create_firebase_user, read_firebase_user, update_firebase_user
 
 
+class FirebaseClientTest(TestCase):
+    def setUp(self):
+        self.uid = "abc123"
+        self.email = "foo@bar.org"
+        self.email_verified = True
+        self.picture = "https://www.bar.org/image.jpg"
+        self.name = "Foo Bar"
+        self.sign_in_provider = "password"
+        self.decoded_token_no_uid_or_email = {
+            "uid": None,
+            "email": None,
+        }
+        self.decoded_token_valid = {
+            "uid": self.uid,
+            "email": self.email,
+            "email_verified": self.email_verified,
+            "picture": self.picture,
+            "name": self.name,
+            "firebase": {"sign_in_provider": self.sign_in_provider},
+        }
+
+    def test_get_token_invalid(self):
+        with self.assertLogs("firebase.clients", level="ERROR"):
+            get_token_info("")
+
+    @patch("firebase.clients.auth.verify_id_token")
+    def test_get_token_no_uid_or_email(self, mock_verify_id_token: MagicMock):
+        mock_verify_id_token.return_value = self.decoded_token_no_uid_or_email
+        info = get_token_info("garbage")
+        self.assertIsNone(info)
+
+    @patch("firebase.clients.auth.verify_id_token")
+    def test_get_token_valid(self, mock_verify_id_token: MagicMock):
+        mock_verify_id_token.return_value = self.decoded_token_valid
+        info = get_token_info("garbage")
+        self.assertEqual(self.uid, info["username"])
+        self.assertEqual(self.email, info["email"])
+        self.assertEqual(self.email_verified, info["email_verified"])
+        self.assertEqual(self.picture, info["photo_url"])
+        self.assertEqual(self.name, info["display_name"])
+        self.assertEqual(self.sign_in_provider, info["provider"])
+
+
+class CRUDTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="ash",
+            password="pikachu",  # noqa: S106 (Hard-coded for testing.)
+            email="ash@kantomail.net",
+        )
+
+    def test_create_non_existent_user(self):
+        create_firebase_user("misty", "misty@pokemail.com")
+        self.assertTrue(User.objects.filter(username="misty").exists())
+
+    def test_create_existing_user(self):
+        user = create_firebase_user(self.user.username, self.user.email)
+        self.assertTrue(User.objects.filter(username=self.user.username).exists())
+        self.assertEqual(self.user.username, user.username)
+
+    def test_read_non_existent_user(self):
+        user = read_firebase_user("brock")
+        self.assertIsNone(user)
+
+    def test_read_existing_user(self):
+        user = read_firebase_user("ash")
+        self.assertTrue(User.objects.filter(username=self.user.username).exists())
+        self.assertEqual(self.user.username, user.username)
+
+    def test_update_non_existent_user(self):
+        user = update_firebase_user("oak", "oak@kanto.edu")
+        self.assertIsNone(user)
+
+    def test_update_existing_user(self):
+        new_email = "aketchum@kantomail.net"
+        user = update_firebase_user("ash", new_email)
+        self.assertEqual(new_email, user.email)
+
+
 class VerifyViewTest(TestCase):
     def setUp(self):
         self.client = Client()
@@ -174,82 +253,3 @@ class EditProfileViewTest(TestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("login"))
-
-
-class FirebaseClientTest(TestCase):
-    def setUp(self):
-        self.uid = "abc123"
-        self.email = "foo@bar.org"
-        self.email_verified = True
-        self.picture = "https://www.bar.org/image.jpg"
-        self.name = "Foo Bar"
-        self.sign_in_provider = "password"
-        self.decoded_token_no_uid_or_email = {
-            "uid": None,
-            "email": None,
-        }
-        self.decoded_token_valid = {
-            "uid": self.uid,
-            "email": self.email,
-            "email_verified": self.email_verified,
-            "picture": self.picture,
-            "name": self.name,
-            "firebase": {"sign_in_provider": self.sign_in_provider},
-        }
-
-    def test_get_token_invalid(self):
-        with self.assertLogs("firebase.clients", level="ERROR"):
-            get_token_info("")
-
-    @patch("firebase.clients.auth.verify_id_token")
-    def test_get_token_no_uid_or_email(self, mock_verify_id_token: MagicMock):
-        mock_verify_id_token.return_value = self.decoded_token_no_uid_or_email
-        info = get_token_info("garbage")
-        self.assertIsNone(info)
-
-    @patch("firebase.clients.auth.verify_id_token")
-    def test_get_token_valid(self, mock_verify_id_token: MagicMock):
-        mock_verify_id_token.return_value = self.decoded_token_valid
-        info = get_token_info("garbage")
-        self.assertEqual(self.uid, info["username"])
-        self.assertEqual(self.email, info["email"])
-        self.assertEqual(self.email_verified, info["email_verified"])
-        self.assertEqual(self.picture, info["photo_url"])
-        self.assertEqual(self.name, info["display_name"])
-        self.assertEqual(self.sign_in_provider, info["provider"])
-
-
-class CRUDTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(
-            username="ash",
-            password="pikachu",  # noqa: S106 (Hard-coded for testing.)
-            email="ash@kantomail.net",
-        )
-
-    def test_create_non_existent_user(self):
-        create_firebase_user("misty", "misty@pokemail.com")
-        self.assertTrue(User.objects.filter(username="misty").exists())
-
-    def test_create_existing_user(self):
-        user = create_firebase_user(self.user.username, self.user.email)
-        self.assertTrue(User.objects.filter(username=self.user.username).exists())
-        self.assertEqual(self.user.username, user.username)
-
-    def test_read_non_existent_user(self):
-        user = read_firebase_user("brock")
-        self.assertIsNone(user)
-
-    def test_read_existing_user(self):
-        user = read_firebase_user("ash")
-        self.assertTrue(User.objects.filter(username=self.user.username).exists())
-        self.assertEqual(self.user.username, user.username)
-
-    def test_update_non_existent_user(self):
-        user = update_firebase_user("oak", "oak@kanto.edu")
-        self.assertIsNone(user)
-
-    def test_update_existing_user(self):
-        new_email = "aketchum@kantomail.net"
-        user = update_firebase_user("ash", new_email)
-        self.assertEqual(new_email, user.email)
