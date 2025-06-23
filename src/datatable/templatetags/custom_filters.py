ac5a8b0874fccee461c143f7a0398e45b499fc58
Liam Mulhall <liammulh@gmail.com>
2025-06-23 13:41:12 -0700

    wip: Add sorting

--- src/datatable/templatetags/custom_filters.py
+++ src/datatable/templatetags/custom_filters.py
@@ -3,6 +3,7 @@
 from typing import Any
 
 from django.db.models import Model
+from django.http import HttpRequest
 from django.template.defaulttags import register
 
 
@@ -18,3 +19,9 @@ def get_val(model: Model, field_name: str) -> Any | str:  # noqa: ANN401 (We don
 def get_item(dictionary: dict, key: str) -> Any | str:  # noqa: ANN401 (We don't know the type of the value.)
     """Returns the value to a key in a dictionary."""
     return dictionary.get(key, "")
+
+
+@register.filter
+def in_get(value: str, request: HttpRequest) -> bool:
+    """Returns whether the value is present in request.GET."""
+    return value in request.GET
