34a523bac282a92616e79a0f41501416fb50666e
Liam Mulhall <liammulh@gmail.com>
2025-06-24 11:55:12 -0700

    wip: Add test for create view

--- src/publications/tests.py
+++ src/publications/tests.py
@@ -1,5 +1,6 @@
 """Houses tests for the publications app."""
 
+from bs4 import BeautifulSoup
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -10,4 +11,31 @@ class PublicationCreateViewTest(TestCase):
         self.url = reverse("publication-create")
 
     def test_shows_radio_buttons(self):
-        self.client.get(self.url)
+        response = self.client.get(self.url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        pubmed_radio = (
+            soup.find("label", {"for": "id_publication_type_0"}).get_text().strip()
+        )
+        self.assertIn("PubMed", pubmed_radio)
+        biorxiv_radio = (
+            soup.find("label", {"for": "id_publication_type_1"}).get_text().strip()
+        )
+        self.assertIn("bioRxiv", biorxiv_radio)
+        medrxiv_radio = (
+            soup.find("label", {"for": "id_publication_type_2"}).get_text().strip()
+        )
+        self.assertIn("medRxiv", medrxiv_radio)
+
+    def test_shows_inputs(self):
+        response = self.client.get(self.url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        pubmed_input = soup.find(id="id_pubmed_id")
+        self.assertIsNotNone(pubmed_input)
+        doi_input = soup.find(id="id_doi")
+        self.assertIsNotNone(doi_input)
+
+    def test_shows_submit_button(self):
+        response = self.client.get(self.url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        submit_button = soup.find("button", {"type": "submit"}).get_text().strip()
+        self.assertEqual(submit_button, "Submit")
