902e1c3df067947fee7866a1ab207e779cf5cca3
Liam Mulhall <liammulh@gmail.com>
2025-06-25 18:39:31 -0700

    chore: Rename URL to match other URLs

--- src/config/urls.py
+++ src/config/urls.py
@@ -6,7 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("core.urls")),
     path("admin/", admin.site.urls),
+    path("datatable/", include("datatable.urls")),
     path("firebase/", include("firebase.urls")),
     path("publication/", include("publication.urls")),
-    path("datatable-test/", include("datatable.urls")),
 ]
