605f7adc83a815453bff51f4196cc9b63c290823
Liam Mulhall <liammulh@gmail.com>
2025-07-03 15:39:15 -0700

    wip: Add haplotype search view
    https://github.com/ClinGen/hla-curation-interface/issues/21

--- src/haplotype/views.py
+++ src/haplotype/views.py
@@ -1,9 +1,12 @@
 """Provides views for the haplotype app."""
 
+from django.http import HttpRequest, HttpResponse
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView
 
 from core.permissions import CreateAccessMixin
+from datatable.constants import FieldTypes, SortDirections
+from datatable.views import datatable
 from haplotype.forms import HaplotypeForm
 from haplotype.models import Haplotype
 
@@ -21,3 +24,42 @@ class HaplotypeDetail(DetailView):
 
     model = Haplotype
     template_name = "haplotype/detail.html"
+
+
+# Define fields for use in the datatable.
+FIELDS = [
+    {
+        "text": "ID",
+        "param_name": "pk",
+        "id": "pk",
+        "default_value": "",
+        "type": FieldTypes.SEARCH,
+        "placeholder": "",
+    },
+    {
+        "text": "CAR ID",
+        "param_name": "car_id",
+        "id": "car-id",
+        "default_value": "",
+        "type": FieldTypes.SEARCH,
+        "placeholder": "",
+    },
+    {
+        "text": "Added",
+        "param_name": "added_at",
+        "id": "added-at",
+        "default_value": SortDirections.DEFAULT,
+        "type": FieldTypes.SORT,
+    },
+]
+
+
+def haplotype_search(request: HttpRequest) -> HttpResponse:
+    """Returns an interactive datatable for searching haplotypes."""
+    return datatable(
+        request=request,
+        model=Haplotype,
+        order_by="pk",
+        fields=FIELDS,  # type: ignore
+        data_title="Haplotypes",
+    )
