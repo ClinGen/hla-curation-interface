8896919b45f9a44d289f8f904c9788440fb40152
Liam Mulhall <liammulh@gmail.com>
2025-07-20 17:56:43 -0700

    wip: Add tests for evidence model
    https://github.com/ClinGen/hla-curation-interface/issues/22

--- src/curation/tests/test_models.py
+++ src/curation/tests/test_models.py
@@ -4,8 +4,19 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from allele.models import Allele
-from curation.models import Classification, Curation, CurationTypes, Status
+from curation.models import (
+    Classification,
+    Curation,
+    CurationTypes,
+    Demographic,
+    Evidence,
+    Points,
+    Status,
+    TypingMethod,
+    Zygosity,
+)
 from disease.models import Disease
+from publication.models import Publication
 
 
 class TestCuration(TestCase):
@@ -43,3 +54,80 @@ class TestCuration(TestCase):
                 disease=self.disease,
             )
             curation.clean()
+
+
+class TestEvidence(TestCase):
+    fixtures = [
+        "test_alleles.json",
+        "test_diseases.json",
+        "test_curations.json",
+        "test_publications.json",
+        "demographics.json",
+    ]
+
+    def setUp(self):
+        self.allele = Allele.objects.get(pk=1)
+        self.disease = Disease.objects.get(pk=1)
+        self.curation = Curation.objects.get(pk=1)
+        self.publication = Publication.objects.get(pk=1)
+        self.evidence = Evidence(
+            curation=self.curation,
+            publication=self.publication,
+        )
+        self.evidence.save()
+
+    def test_status_is_in_progress_when_created(self):
+        self.assertEqual(self.evidence.status, Status.IN_PROGRESS)
+
+    def test_conflicting_is_false_when_created(self):
+        self.assertFalse(self.evidence.is_conflicting)
+
+    def test_included_is_false_when_created(self):
+        self.assertFalse(self.evidence.is_included)
+
+    def test_gwas_is_false_when_created(self):
+        self.assertFalse(self.evidence.is_gwas)
+
+    def test_is_monoallelic_when_created(self):
+        self.assertEqual(self.evidence.zygosity, Zygosity.MONOALLELIC)
+
+    def test_score_increases_when_biallelic_selected(self):
+        initial_points = self.evidence.score
+        self.evidence.zygosity = Zygosity.BIALLELIC
+        self.evidence.save()
+        self.assertGreater(self.evidence.score, initial_points)
+        self.assertEqual(self.evidence.score, initial_points + Points.S1C_BIALLELIC)
+
+    def test_phase_confirmed_is_false_when_created(self):
+        self.assertFalse(self.evidence.phase_confirmed)
+
+    def test_score_increases_when_phase_confirmed(self):
+        initial_points = self.evidence.score
+        self.evidence.phase_confirmed = True
+        self.evidence.save()
+        self.assertGreater(self.evidence.score, initial_points)
+        self.assertEqual(
+            self.evidence.score, initial_points + Points.S1D_PHASE_CONFIRMED
+        )
+
+    def test_typing_method_is_empty_when_created(self):
+        self.assertEqual(self.evidence.typing_method, "")
+
+    def test_score_increases_when_typing_method_selected(self):
+        initial_points = self.evidence.score
+        self.evidence.typing_method = TypingMethod.LONG_READ_SEQ
+        self.evidence.save()
+        self.assertGreater(self.evidence.score, initial_points)
+        self.assertEqual(self.evidence.score, initial_points + Points.S2_LONG_READ_SEQ)
+
+    def test_demographics_is_empty_when_created(self):
+        self.assertEqual(len(self.evidence.demographics.all()), 0)
+
+    def test_can_add_demographic(self):
+        demographic = Demographic.objects.get(pk=1)
+        self.evidence.demographics.add(demographic)
+        self.evidence.save()
+        self.assertIn(demographic, self.evidence.demographics.all())
+
+    def test_p_value_is_empty_when_created(self):
+        self.assertIsNone(self.evidence.p_value)
