2822ae23c9f76ba30e8e261bbd890981e5a86d4c
Liam Mulhall <liammulh@gmail.com>
2026-01-13 12:09:44 -0800

    chore: Fix various CI issues
    https://github.com/ClinGen/hla-curation-interface/pull/53

--- src/common/context_processors.py
+++ src/common/context_processors.py
@@ -3,6 +3,6 @@
 from django.conf import settings
 
 
-def git_sha(request) -> dict:  # noqa: ANN001 (Required to have the param.)
+def git_sha(request) -> dict:  # noqa (Required to have the param.)
     """Returns the shortened Git SHA."""
     return {"GIT_SHA": settings.GIT_SHA}
